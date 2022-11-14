a0e801df4d5a5012d92561338b2444dab2ba4840
vinicius <viniciusribeiro.dev@hotmail.com>
2022-11-14 18:26:45 -0300

    Projeto pronto e funcionando

--- js/database.js
+++ js/database.js
@@ -3,62 +3,56 @@
 const data = [
   {
     id: 1,
-    img: "../img/jaqueta.svg",
-    nameItem: "Lightweight Jacket",
-    description:
-      "Adicione um pouco de energia ao seu guarda-roupa de inverno com esta jaqueta vibrante...",
-    value: 100,
-    addCart: "Adicionar ao carrinho",
-    tag: ["Camisetas"],
+    img: "./../img/Violão_Acústico_Anatômico.webp",
+    nome: "Violão Acústico Anatômico J. White Folk Phx Black Satin",
+    descricao:
+      "A linha de violões anatômicos J. White é uma série que possui um corpo que se adapta perfeitamente ao braço do músico. A borda superior direita do tampo é levemente inclinada facilitando o apoio do braço direito no instrumento, dando maior conforto durante o uso.",
+    preco: 1290,
+    tag: ["Violão"],
   },
   {
     id: 2,
-    img: "../img/gorro.svg",
-    nameItem: "Black Hat",
-    description:
-      "O gorro Next.js chegou! Esta beldade bordada tem um ajuste confortável que garante que...",
-    value: 100,
-    addCart: "Adicionar ao carrinho",
-    tag: ["Acessórios"],
+    img: "./../img/lavame2.webp",
+    nome: "Violão de Fibra de Carbono LAVA ME 2",
+    descricao:
+      "A LAVA acredita que o futuro do violão pode ser melhor do que o passado. Que ele pode ser mais leve e mais simples. Um futuro onde você não precisa se preocupar com as condições do clima, apenas em aproveitar a sonoridade incrivelmente superior do seu instrumento.Esse futuro já chegou ao Brasil.",
+    preco: 4031,
+    tag: ["Violão"],
   },
   {
     id: 3,
-    img: "../img/mascara.svg",
-    nameItem: "Mask",
-    description:
-      "Esta máscara facial durável é feita de duas camadas de tecido tratado e possui presilhas...",
-    value: 40,
-    addCart: "Adicionar ao carrinho",
-    tag: ["Acessórios"],
+    img: "./../img/Violão_TransAcoustic_Yamaha_CG-TA.webp",
+    nome: "Violão TransAcoustic Yamaha CG-TA Nylon Natural",
+    descricao:
+      "Com mais de um século na fabricação de instrumentos musicais, a Yamaha é uma marca que fala por si mesma. Sendo uma referência mundial na indústria musical, o seu selo de qualidade nos violões é um diferencial garantido para suas canções. Som amplificado Sendo eletroacústico, é possível obter um melhor som e aumentado ao conectá-lo a um sistema de alto-falantes. Um modelo para cada guitarrista A tampa de abeto gera um tom brilhante e claro, mesmo nos registros mais agudos. Qualidade se destaca As cordas de nylon são caracterizadas por serem flexíveis e resistentes. Garantem a durabilidade e além disso têm um grande poder de absorção da carga de choque.",
+    preco: 5010,
+    tag: ["Violão"],
   },
   {
     id: 4,
-    img: "../img/camiseta_preta.svg",
-    nameItem: "T-Shirt",
-    description:
-      "Esta t-shirt é imprescindível no seu guarda-roupa, combinando o caimento intemporal de...",
-    value: 100,
-    addCart: "Adicionar ao carrinho",
-    tag: ["Camisetas"],
+    img: "./../img/piano_de_cauda_gx-6.webp",
+    nome: "piano de cauda kawai gx-6",
+    descricao:
+      "O KAWAI GX-6 é um piano de cauda de 88 teclas, 3 pedais, 214 cm de profundidade, tábua harmônica de Spruce maciço com área de 1,60 m2, tampa do teclado com amortecedor hidráulico, tampa superior com três posições de abertura, mecanismo Millennium III em ABS-Carbono, teclado em Spruce maciço com coberturas em NEOTEX™('marfim' e 'ébano' sintéticos), martelos com cabeças de mogno e feltro duplo de pura lã resistente à umidade, Dual Duplex Scale, acabamento Alto Brilho em Preto. Acompanha banqueta banqueta regulável. Garantia de 5 anos. ",
+    preco: 316316,
+    tag: ["Piano"],
   },
   {
     id: 5,
-    img: "../img/camiseta_branca.svg",
-    nameItem: "Short-Sleeve T-Shirt",
-    description:
-      "Agora você encontrou a camiseta básica do seu guarda-roupa. É feito de um mais grosso...",
-    value: 100,
-    addCart: "Adicionar ao carrinho",
-    tag: ["Camisetas"],
+    img: "./../img/Piano_De_Cauda_Branco_Cenográfico.webp",
+    nome: "Piano De Cauda Branco Cenográfico 1/2 P/ Pianos Digitais",
+    descricao:
+      "Os pianos cenográficos CAZIUK esta em alta entre os artistas, por ser leve, fácil de transportar, não desafina, produto durável, surpreende com a sua presença, dando um glamour onde se encontra, um item de decoração de luxo, seu brilho atraia a atenção de todos, é bem requisitado em festas de casamento, e outros eventos.",
+    preco: 19100,
+    tag: ["Piano"],
   },
   {
     id: 6,
-    img: "../img/moletom.svg",
-    nameItem: "Champion Packable Jacket",
-    description:
-      "Proteja-se dos elementos com esta jaqueta embalável Champion. Esta jaqueta de poliést...",
-    value: 100,
-    addCart: "Adicionar ao carrinho",
-    tag: ["Camisetas"],
+    img: "./../img/casio_px-870_privia.webp",
+    nome: "casio px-870 privia piano digital branco",
+    descricao:
+      "As teclas que imitam ébano e marfim propiciam a elegante sensação e textura do suave toque do teclado de um piano de cauda. O acabamento talhado e minuciosamente detalhado da superfície impede que o dedo escorregue em decorrência de suor, proporcionando a sensação de uma perfeita adequação para as pontas dos dedos, mesmo ao tocar por períodos prolongados.",
+    preco: 6190,
+    tag: ["Piano"],
   },
 ];
