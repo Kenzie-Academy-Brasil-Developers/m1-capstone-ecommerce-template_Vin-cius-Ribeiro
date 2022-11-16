08c9b5f60fe4c8bb9f0986e5f3c9fbebfb462857
vinicius <viniciusribeiro.dev@hotmail.com>
2022-11-16 15:39:27 -0300

    correção img

--- js/index.js
+++ js/index.js
@@ -1,4 +1,3 @@
-
 let ul = document.querySelector(".lista_vitrine");
 
 let todos = document.querySelector(".todos");
@@ -9,13 +8,17 @@ let madeira = document.querySelector(".madeira");
 
 let eletrico = document.querySelector(".eletrico");
 
-let sopro = document.querySelector(".sopro")
+let sopro = document.querySelector(".sopro");
 
-let metais = document.querySelector(".matais")
+let metais = document.querySelector(".matais");
 
-let percussao = document.querySelector(".percussao")
+let percussao = document.querySelector(".percussao");
 
-let acessorios = document.querySelector(".acessorios")
+let acessorios = document.querySelector(".acessorios");
+
+let section = document.querySelector(".select");
+
+console.log(section.value);
 
 let button_ = document.querySelectorAll(".adicionar_carrinho");
 
@@ -39,7 +42,10 @@ let pesquisa = document.querySelector(".digite");
 
 let buttonPesquisa = document.querySelector(".pesquisar");
 
-let formatador = new Intl.NumberFormat("pt-BR", {style: "currency", currency: "BRL"});
+let formatador = new Intl.NumberFormat("pt-BR", {
+  style: "currency",
+  currency: "BRL",
+});
 
 for (let i = 0; i < data.length; i++) {
   let li = document.createElement("li");
@@ -107,7 +113,7 @@ todos.addEventListener("click", (e) => {
     p.innerHTML = data[i].descricao;
 
     let span_preco = document.createElement("span");
-    
+
     span_preco.innerHTML = formatador.format(data[i].preco);
 
     let button = document.createElement("button");
@@ -135,11 +141,11 @@ todos.addEventListener("click", (e) => {
   }
 });
 
-cordas.addEventListener("click", (e) => {
+section.addEventListener("click", (e) => {
   ul.innerHTML = "";
   for (let i = 0; i < data.length; i++) {
-    let tag = data[i].tag
-    if (tag.includes("Cordas")) {
+    let tag = data[i].tag;
+    if (tag.includes(section.value) || section.value == "Categorias") {
       let li = document.createElement("li");
       li.classList = "itens";
       li.setAttribute("id", `${i + 1}`);
@@ -158,7 +164,7 @@ cordas.addEventListener("click", (e) => {
       p.innerHTML = data[i].descricao;
 
       let span_preco = document.createElement("span");
-      span_preco.innerHTML = formatador.format(data[i].preco);;
+      span_preco.innerHTML = formatador.format(data[i].preco);
 
       let button = document.createElement("button");
       button.innerHTML = "Adicionar ao carrinho";
@@ -184,7 +190,105 @@ cordas.addEventListener("click", (e) => {
     }
   }
 });
+function removerCarrinho(target) {
+  target.remove();
+  totalItens_--;
+  totalItens.innerText = totalItens_;
+  if (totalItens_ == 0) {
+    total.style.display = "none";
+    carrinhoVazio.style.display = "flex";
+  }
+}
 
+function addCarrinho(produto) {
+  total.style.display = "flex";
+
+  let li_carrinho = document.createElement("li");
+  li_carrinho.classList = "itens_carrinho";
+  li_carrinho.setAttribute("id", `${produto.id}`);
+
+  let img_carrinho = document.createElement("img");
+  img_carrinho.src = produto.img;
+
+  let div = document.createElement("div");
+
+  let h3_carrinho = document.createElement("h3");
+  h3_carrinho.innerHTML = produto.nome;
+
+  let button_carrinho = document.createElement("button");
+  button_carrinho.classList = "remover";
+  button_carrinho.innerHTML = "Remover produto";
+
+  carrinhoVazio.style.display = "none";
+
+  totalItens_++;
+
+  totalItens.innerText = totalItens_;
+
+  precoTotal += produto.preco;
+
+  let precoFormatado = formatador.format(precoTotal);
+
+  let span_carrinho = document.createElement("span");
+  span_carrinho.innerHTML = formatador.format(produto.preco);
+
+  div.append(h3_carrinho, span_carrinho, button_carrinho);
+
+  li_carrinho.append(img_carrinho, div);
+
+  ul_.append(li_carrinho);
+
+  preco.innerText = precoFormatado;
+
+  button_carrinho.addEventListener("click", (e) => {
+    removerCarrinho(li_carrinho);
+    precoTotal -= produto.preco;
+    preco.innerText = formatador.format(precoTotal);
+  });
+}
+
+buttonPesquisa.addEventListener("click", (e) => {
+  let filtroDePesquisa = pesquisa.value.toLowerCase();
+  ul.innerHTML = "";
+  for (let i = 0; i < data.length; i++) {
+    const nome = data[i].nome.toLowerCase();
+    if (nome.includes(filtroDePesquisa)) {
+      let li = document.createElement("li");
+      li.classList = "itens";
+
+      let img = document.createElement("img");
+      img.src = data[i].img;
+
+      let span = document.createElement("span");
+      span.classList = "class";
+      span.innerHTML = data[i].tag;
+
+      let h3 = document.createElement("h3");
+      h3.innerHTML = data[i].nome;
+
+      let p = document.createElement("p");
+      p.innerHTML = "Descrição aqui";
+      p.innerHTML = data[i].descricao;
+
+      let span_preco = document.createElement("span");
+      span_preco.innerHTML = formatador.format(data[i].preco);
+
+      let button = document.createElement("button");
+      button.innerHTML = "Adicionar ao carrinho";
+      button.classList = "adicionar_carrinho";
+
+      li.append(img, span, h3, p, span_preco, button);
+
+      ul.append(li);
+
+      button.addEventListener("click", (e) => {
+        addCarrinho(data[i]);
+      });
+    }
+  }
+});
+
+/*
 madeira.addEventListener("click", (e) => {
   ul.innerHTML = "";
   for (let i = 0; i < data.length; i++) {
@@ -490,103 +594,4 @@ acessorios.addEventListener("click", (e) => {
     }
   }
 });
-
-function removerCarrinho(target) {
-  target.remove();
-  totalItens_--;
-  totalItens.innerText = totalItens_;
-  if (totalItens_ == 0) {
-    total.style.display = "none";
-    carrinhoVazio.style.display = "flex";
-  }
-}
-
-function addCarrinho(produto) {
-  total.style.display = "flex";
-
-  let li_carrinho = document.createElement("li");
-  li_carrinho.classList = "itens_carrinho";
-  li_carrinho.setAttribute("id", `${produto.id}`);
-
-  let img_carrinho = document.createElement("img");
-  img_carrinho.src = produto.img;
-
-  let div = document.createElement("div");
-
-  let h3_carrinho = document.createElement("h3");
-  h3_carrinho.innerHTML = produto.nome;
-
-
-  let button_carrinho = document.createElement("button");
-  button_carrinho.classList = "remover";
-  button_carrinho.innerHTML = "Remover produto";
-
-
-  carrinhoVazio.style.display = "none";
-
-  totalItens_++;
-
-  totalItens.innerText = totalItens_;
-
-  precoTotal += produto.preco;
-
-  let precoFormatado = formatador.format(precoTotal)
-
-  let span_carrinho = document.createElement("span");
-  span_carrinho.innerHTML = formatador.format(produto.preco);
-
-  div.append(h3_carrinho, span_carrinho, button_carrinho);
-
-  li_carrinho.append(img_carrinho, div);
-
-  ul_.append(li_carrinho);
-
-  preco.innerText = precoFormatado;
-
-  button_carrinho.addEventListener("click", (e) => {
-    removerCarrinho(li_carrinho);
-    precoTotal -= produto.preco;
-    preco.innerText = formatador.format(precoTotal);
-  });
-}
-
-buttonPesquisa.addEventListener("click", (e) => {
-  let filtroDePesquisa = pesquisa.value.toLowerCase();
-  ul.innerHTML = "";
-  for (let i = 0; i < data.length; i++) {
-    const nome = data[i].nome.toLowerCase();
-    if (nome.includes(filtroDePesquisa)) {
-      let li = document.createElement("li");
-      li.classList = "itens";
-
-      let img = document.createElement("img");
-      img.src = data[i].img;
-
-      let span = document.createElement("span");
-      span.classList = "class";
-      span.innerHTML = data[i].tag;
-
-      let h3 = document.createElement("h3");
-      h3.innerHTML = data[i].nome;
-
-      let p = document.createElement("p");
-      p.innerHTML = "Descrição aqui";
-      p.innerHTML = data[i].descricao;
-
-      let span_preco = document.createElement("span");
-      span_preco.innerHTML = formatador.format(data[i].preco);;
-
-      let button = document.createElement("button");
-      button.innerHTML = "Adicionar ao carrinho";
-      button.classList = "adicionar_carrinho";
-
-      li.append(img, span, h3, p, span_preco, button);
-
-      ul.append(li);
-
-      button.addEventListener("click", (e) => {
-        addCarrinho(data[i]);
-      });
-    }
-  }
-});
+*/
